61433ebb7d708339c5374e44df1b91d4fe3c31d5
Jiuyue1999 <wang.11537@buckeyemail.osu.edu>
2022-02-01 16:18:30 -0500

    Add Health bar and Bullet Damage System

---
+++ Assets/CSE5912/Character/DamageController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class DamageController : MonoBehaviour
+    {
+        [SerializeField] private int bulletDamage;
+
+        [SerializeField] private HealthSystem healthSystem;
+
+        //private void OnTriggerEnter(Collider other)
+        //{
+        //    Debug.Log("Hit!!");
+        //    if (other.CompareTag("Bullet"))
+        //    {
+        //        healthSystem.Damage(50);
+        //        if (healthSystem.getHealth() == 0)
+        //        {
+        //            gameObject.SetActive(false);
+        //        }
+        //    }
+        //}
+
+        private void OnCollisionEnter(Collision collisionInfo)
+        {
+            Debug.Log("Hit!!" + collisionInfo.collider.tag);
+            if (collisionInfo.collider.tag == "Bullet")
+            {
+                healthSystem.Damage(bulletDamage);
+                if (healthSystem.getHealth() == 0)
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+
+    }
+
