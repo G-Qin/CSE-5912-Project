d7b0bf5b55c122d572a6743584f96ee3d9776e26
ZinaZhang <zhang.9485@buckeyemail.osu.edu>
2022-02-15 16:24:16 -0500

    Loading&Climb

--- Assets/Menu_Scene/ButtonManagerScript.cs
+++ Assets/Menu_Scene/ButtonManagerScript.cs
@@ -1,15 +1,18 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonManagerScript : MonoBehaviour
 {
+    public GameObject LoadingPanel;
    
     public void StartGame()
     {
+        LoadingPanel.SetActive(true);
         Debug.Log("Starting game");
         SceneManager.LoadScene("Map_v7");
-    }
 
+    }
     public void ExitGame()
     {
         Debug.Log("Quitting game");
