c7990f2c8916dfe64c208c261263477f392a373a
Liangcan-Li <70410241+Liangcan-Li@users.noreply.github.com>
2022-02-23 15:55:47 -0500

    Revert "Tb4 llc"

--- Assets/CSE5912/Character/DamageController.cs
+++ Assets/CSE5912/Character/DamageController.cs
@@ -1,19 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class DamageController : MonoBehaviour
-{
-    [SerializeField] private int bulletDamage;
-
-    [SerializeField] private HealthSystem healthSystem;
-    public Animator anim;
-    public NavMeshAgent enemy;
-    void Start()
+    public class DamageController : MonoBehaviour
     {
-        anim = GetComponent<Animator>();
-    }
+        [SerializeField] private int bulletDamage;
+
+        [SerializeField] private HealthSystem healthSystem;
+
     //private void OnTriggerEnter(Collider other)
     //{
     //    Debug.Log("Hit!!");
@@ -26,34 +20,20 @@ public class DamageController : MonoBehaviour
     //        }
     //    }
     //}
-
+    
     private void OnCollisionEnter(Collision collisionInfo)
-    {
-        //Debug.Log("Hit!!" + collisionInfo.collider.tag);
-        if (collisionInfo.collider.tag == "Bullet")
         {
-            healthSystem.Damage(bulletDamage);
-            if (healthSystem.getHealth() == 0)
+            //Debug.Log("Hit!!" + collisionInfo.collider.tag);
+            if (collisionInfo.collider.tag == "Bullet")
             {
-                enemy.enabled = false;
-                anim.SetBool("dead", true);
-                //StartCoroutine("waiter");
-                //gameObject.SetActive(false);
-                //Destroy(gameObject);
-                Destroy(gameObject, 3.0f);
-            }
-            else
-            {
-                anim.SetBool("dead", false);
+                healthSystem.Damage(bulletDamage);
+                if (healthSystem.getHealth() == 0)
+                {
+                    gameObject.SetActive(false);
+                    Destroy(gameObject);
+                }
             }
         }
 
     }
-    IEnumerator waiter()
-    {
-        yield return new WaitForSeconds(0.1f);
-    }
-
-
-}
 
