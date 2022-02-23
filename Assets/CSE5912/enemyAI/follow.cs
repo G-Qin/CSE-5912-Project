4cb979e25673b1a3be0bc2418daf942d216c6c11
Liangcan-Li <li.9571@osu.edu>
2022-02-23 15:55:05 -0500

    test changes

--- Assets/CSE5912/enemyAI/follow.cs
+++ Assets/CSE5912/enemyAI/follow.cs
@@ -10,12 +10,14 @@ public class follow : MonoBehaviour
     [SerializeField] private int bulletDamage;
     [SerializeField] private HealthSystem healthSystem;
     private bool live;
+    Collider m_Collider;
     void Start()
     {
         anim = GetComponent<Animator>();
         live = true;
         Player =GameObject.Find("/P_LPSP_FP_CH_1").transform;
         StartCoroutine("waiter");
+        m_Collider = GetComponent<Collider>();
 
         //Player=GameObject.Find("/P_LPSP_FP_CH_1/SK_FP_CH_Default_Root/Armature/root/pelvis/spine_01/spine_02/spine_03/neck_01/head/SOCKET_Camera/Camera/Camera Depth");
     }
