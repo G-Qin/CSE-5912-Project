63280c0a3564721e895c14ca1bd398227ca746d8
CYBuckeye <97851137+CYBuckeye@users.noreply.github.com>
2022-04-04 10:36:09 -0400

    4/3

---
+++ Assets/CSE5912/Character/BulletDamageSystem.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamageSystem : MonoBehaviour
+{
+    int bulletDamage;
+    int grenadeDamage;
+    int GlDamage;
+
+    void Start()
+    {
+        bulletDamage = 20;
+        grenadeDamage = 50;
+        GlDamage = 40;
+    }
+
+    public void setBulletDamage(int i)
+    {
+        bulletDamage = i;
+    }
+
+    public int getBUlletDamage()
+    {
+        return bulletDamage;
+    }
+
+    public void setGlDamage(int i)
+    {
+        GlDamage = i;
+    }
+
+    public int getGlDamage()
+    {
+        return GlDamage;
+    }
+
+    public void setgrenadeDamage(int i)
+    {
+        grenadeDamage = i;
+    }
+
+    public int getgrenadeDamage()
+    {
+        return grenadeDamage;
+    }
+
+
+
+}
