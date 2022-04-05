0210f94cf57b4518671d92da73e8929fc2d0df0c
CYBuckeye <chen.8185@buckeyemail.osu.edu>
2022-04-05 16:48:56 -0400

    Update

--- Assets/CSE5912/Character/CDamageSystem.cs
+++ Assets/CSE5912/Character/CDamageSystem.cs
@@ -5,8 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class CDamageSystem : MonoBehaviour
 {
-    private int weaponDamage = 15;
-    private int fireRain = 10;
+    public Animation anim;
+    private int weaponDamage = 19;
+    private int fireRain = 5;
+    private int DragonBugDamage = 10;
+    private int DragonDamage = 25;
 
     [SerializeField] private HealthSystem healthSystem;
 
@@ -20,9 +23,25 @@ public class CDamageSystem : MonoBehaviour
                 SceneManager.LoadScene("GameOverScene");
             }
         }
-        if (collisionInfo.collider.tag == "FireRain")
+        /*        if (collisionInfo.collider.tag == "DragonBug")
+                {
+                    anim = collisionInfo.collider.gameObject.GetComponent<Animation>();
+                    if (anim.IsPlaying("Attack1"))
+                    {
+                        healthSystem.Damage(DragonBugDamage);
+                        if (healthSystem.getHealth() == 0)
+                        {
+                            SceneManager.LoadScene("GameOverScene");
+                        }
+                    }
+                }*/
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "FireRain")
         {
-            healthSystem.Damage(weaponDamage);
+            healthSystem.Damage(fireRain);
             if (healthSystem.getHealth() == 0)
             {
                 SceneManager.LoadScene("GameOverScene");
@@ -30,7 +49,52 @@ public class CDamageSystem : MonoBehaviour
         }
     }
 
-    
+        
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.tag == "DragonBug")
+        {
+                
+            anim = other.gameObject.GetComponent<Animation>();
+            if (anim.IsPlaying("Attack1"))
+            {
+                if (timeRemaining <= 0)
+                {
+                    Debug.Log("Dragon 111111111");
+                    healthSystem.Damage(DragonBugDamage);
+                    if (healthSystem.getHealth() == 0)
+                    {
+                        SceneManager.LoadScene("GameOverScene");
+                    }
+                    timeRemaining = 1.3f;
+                }
+            }
+        }
+    }
+
+    public float timeRemaining = 0;
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+    }
+
+    private void OnParticleCollision(GameObject collision)
+    {
+        if (collision.CompareTag("DragonDamage")) 
+        {
+            healthSystem.Damage(DragonDamage);
+            if (healthSystem.getHealth() == 0)
+            {
+                SceneManager.LoadScene("GameOverScene");
+            }
+        }
+    }
+
 
 
 }
